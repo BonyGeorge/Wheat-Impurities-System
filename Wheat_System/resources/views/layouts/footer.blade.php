4e5f685faf009945205a947ffc9f88722f6ac280
BonyGeorge <abanoub-g-lamie@hotmail.com>
2020-12-13 12:45:45 +0200

    Added Laravel System

---
+++ Wheat_System/resources/views/layouts/footer.blade.php
@@ -0,0 +1,108 @@
+<head>
+    <link rel="icon" href="img/mdb-favicon.ico" type="image/x-icon">
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.11.2/css/all.css">
+  <!-- Google Fonts Roboto -->
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap">
+  <!-- Bootstrap core CSS -->
+  <link rel="stylesheet" href="{{asset('css/bootstrap.min.css')}}">
+  <!-- Material Design Bootstrap -->
+  <link rel="stylesheet" href="{{asset('css/mdb.min.css')}}">
+  <!-- Your custom styles (optional) -->
+  <link rel="stylesheet" href="{{asset('css/style.css')}}">
+</head>
+<!-- Footer -->
+<footer class="page-footer font-small primary-color pt-4 " style="">
+
+    <!-- Footer Links -->
+    <div class="container text-center text-md-left">
+  
+      <!-- Footer links -->
+      <div class="row text-center text-md-left mt-3 pb-3">
+  
+        <!-- Grid column -->
+        <div class="col-md-3 col-lg-3 col-xl-3 mx-auto mt-3">
+          <h6 class="text-uppercase mb-4 font-weight-bold">Sustaino Company</h6>
+          <p>We aim to make the world a sustainable place to live in and create a sustainable future.</p>
+        </div>
+        <!-- Grid column -->
+  
+        <hr class="w-100 clearfix d-md-none">
+  
+        <!-- Grid column -->
+        <hr class="w-100 clearfix d-md-none">
+  
+        <!-- Grid column -->
+        <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mt-3">
+          <h6 class="text-uppercase mb-4 font-weight-bold">Contact</h6>
+          <p>
+            <i class="fas fa-home mr-3"></i>3/86 st.Abdullah Ibn Taher.</p>
+          <p>
+        <p>
+            <i class="fas fa-home mr-3"></i>11768 Nasr City, Cairo, Egyot.</p>
+            <p>
+            <i class="fas fa-envelope mr-3"></i> info.sustaino@gmail.com</p>
+          <p>
+            <i class="fas fa-phone mr-3"></i> + 01554871501</p>
+        </div>
+        <!-- Grid column -->
+  
+      </div>
+      <!-- Footer links -->
+  
+      <hr>
+  
+      <!-- Grid row -->
+      <div class="row d-flex align-items-center">
+  
+        <!-- Grid column -->
+        <div class="col-md-7 col-lg-8">
+  
+          <!--Copyright-->
+          <p class="text-center text-md-left">© 2020 Copyright:
+              <strong> Sustaino Company.</strong>
+            </a>
+          </p>
+  
+        </div>
+        <!-- Grid column -->
+  
+        <!-- Grid column -->
+        <div class="col-md-5 col-lg-4 ml-lg-0">
+  
+          <!-- Social buttons -->
+          <div class="text-center text-md-right">
+            <ul class="list-unstyled list-inline">
+              <li class="list-inline-item">
+                <a class="btn-floating btn-sm rgba-white-slight mx-1" href="https://www.facebook.com/Sustaino.eg">
+                  <i class="fab fa-facebook-f"></i>
+                </a>
+              </li>
+              <li class="list-inline-item">
+                <a class="btn-floating btn-sm rgba-white-slight mx-1" href="https://www.instagram.com/sustaino.eg/">
+                  <i class="fab fa-instagram"></i>
+                </a>
+              </li>
+            </ul>
+          </div>
+  
+        </div>
+        <!-- Grid column -->
+  
+      </div>
+      <!-- Grid row -->
+  
+    </div>
+    <!-- Footer Links -->
+  
+  </footer>
+  <!-- Footer -->
+
+  <!-- jQuery -->
+  <script type="text/javascript" src="{{asset('js/jquery.min.js')}}"></script>
+  <!-- Bootstrap tooltips -->
+  <script type="text/javascript" src="{{asset('js/popper.min.js')}}"></script>
+  <!-- Bootstrap core JavaScript -->
+  <script type="text/javascript" src="{{asset('js/bootstrap.min.js')}}"></script>
+  <!-- MDB core JavaScript -->
+  <script type="text/javascript" src="{{asset('js/mdb.min.js')}}"></script>
